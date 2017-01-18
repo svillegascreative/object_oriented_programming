9c0757bbd73ddb6e1c5151f342ea08b5022d6cb5
sylvia villegas <sylvia@svillegas.com>
2017-01-18 14:36:14 -0500

    Add instance method eats_at

--- exercise2.rb
+++ exercise2.rb
@@ -7,8 +7,21 @@ class Cat
     @meal_time = meal_time
   end
 
+  def eats_at
+    if @meal_time == 0
+      "12 AM"
+    elsif @meal_time < 12
+      "#{@meal_time} AM"
+    else @meal_time >= 12
+      "#{@meal_time - 12} PM"
+    end
+  end
+
 end
 
 
-kevin = Cat.new("Kevin", "tuna", 4)
+kevin = Cat.new("Kevin", "tuna", 17)
 chuck_d = Cat.new("Chuck D", "cat chow", 12)
+
+# test
+puts kevin.eats_at
