87f354e6d37d4bc0a51c4aa893796e6fc765f4a5
sylvia villegas <sylvia@svillegas.com>
2017-01-18 16:49:06 -0500

    Create method quota

--- exercise4.rb
+++ exercise4.rb
@@ -1,4 +1,16 @@
 class Paperboy
   attr_accessor :name, :experience
   attr_reader :earnings
+
+  def initialize(name)
+    @name = name
+    @experience = 0
+    @earnings = 0
+  end
+
+  def quota
+    50 + @experience / 2
+  end
+
+
 end
