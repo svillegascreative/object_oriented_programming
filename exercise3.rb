8899273c8f1598373435e58e244929543ddd18cd
sylvia villegas <sylvia@svillegas.com>
2017-01-18 15:05:35 -0500

    Create instance methods do_battle and restart

--- exercise3.rb
+++ exercise3.rb
@@ -7,4 +7,25 @@ class Player
     @lives = 5
     @score = 0
   end
+
+  def do_battle
+    @health_points -= 1
+
+    if @health_points == 0
+      @lives -= 1
+      @health_points = 10
+    end
+
+    if @lives == 0
+      restart
+    end
+  end
+
+  def restart
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+    @score = 0
+  end
+
 end
