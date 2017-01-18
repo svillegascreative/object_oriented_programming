e3e7a3ed1039c05cc8da8313755e4c064a9a1d76
sylvia villegas <sylvia@svillegas.com>
2017-01-18 14:50:46 -0500

    Add attributes and set initial values

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,10 @@
 class Player
+  attr_accessor :gold_coins, :health_points, :lives, :score
 
+  def initialize
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+    @score = 0
+  end
 end
