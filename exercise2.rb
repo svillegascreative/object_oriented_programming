842f72c460f401674d4d9bade3aeae1514455fd9
sylvia villegas <sylvia@svillegas.com>
2017-01-18 13:46:13 -0500

    Add name, preferred_food, meal_time attribs

--- exercise2.rb
+++ exercise2.rb
@@ -1,3 +1,10 @@
 class Cat
+  attr_accessor :name, :preferred_food, :meal_time
+
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
 
 end
