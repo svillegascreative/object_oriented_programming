0d1c4aa7779075e61c5b96dbb13379550518be34
sylvia villegas <sylvia@svillegas.com>
2017-01-18 18:12:21 -0500

    Create instance method deliver

--- exercise4.rb
+++ exercise4.rb
@@ -12,5 +12,16 @@ class Paperboy
     50 + @experience / 2
   end
 
+  def deliver(start_address, end_address)
+    papers_delivered = end_address - start_address
+
+    if papers_delivered < quota
+      @earnings = papers_delivered * 0.25 - 2.0
+    elsif papers_delivered >= quota
+      @earnings = (0.25 * papers_delivered) + (papers_delivered - quota) * 0.25
+    end
+
+    @experience += papers_delivered
+  end
 
 end
